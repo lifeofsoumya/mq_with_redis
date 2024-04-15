655eb13b252667feaaac7dd5e4565f4da8fc1ed9
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-04-16 00:45:16 +0530

    updaed queue, done function deprecated

--- queue.js
+++ queue.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.json())
 
 const queue = new Queue('imageProcessingQueue', 'redis://localhost:6379');
-
+let counter = 0 // bad practice, use postman pre request script instead
 app.post('/queue', async (req, res) => {
     console.log(req.body, ' req body')
     try {
-        await queue.add("processing", {  id: uuid(), task: req.body.taskData });
+        await queue.add("processing", { task: req.body.taskData, counter: ++counter, id: uuid() });
         res.status(200).send('Task queued successfully.');
     } catch (err) {
         console.error(err);
--- worker.js
+++ worker.js
@@ -1,20 +1,20 @@
 import { Worker } from 'bullmq';
 
 const processingTask = async (job) => {
-    console.log('Processing task:', job.data);
+    console.log('Processing task no:', job.data.counter, ' id: ', job.data.id);
     await new Promise(resolve => setTimeout(resolve, 3000));
-    console.log('Task completed:', job.data);
-    await job.done()
+    console.log('Task completed:', job.data.counter, '\n------');
+    await job.remove()
 }
 
 export const redisOptions = { host: "localhost", port: 6379 };
 
 const worker = new Worker('imageProcessingQueue', processingTask, { connection: redisOptions })
 
-worker.on("completed", (job) => {
-    console.log(`${job.id} has completed!`);
-});
+// worker.on("completed", (job) => {
+//     console.log(`${job.id} has completed!`);
+// });
 
-worker.on("failed", (job, err) => {
-    console.log(`${job.id} has failed with ${err.message}`);
-});
+// worker.on("failed", (job, err) => {
+//     console.log(`${job.id} has failed with ${err.message}`);
+// });
